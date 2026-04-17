e372acd012d5fe80d065fdc7cebebc0d0f38d0aa
Emily Marx <emilykmarx@alumni.stanford.edu>
2026-04-17 11:35:59 -0700

    Progress on nice logging for final output

--- testing/conftamer.go
+++ testing/conftamer.go
@@ -3,19 +3,35 @@ package testing
 import (
 	"encoding/csv"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"runtime"
+	"strings"
+
+	"k8s.io/apimachinery/pkg/util/json"
 )
 
 /*
 Support for ConfTamer logging of parameters, methods, and messages.
 */
 
+const (
+	methodEntryLog = "ENTER CTYPES METHOD"
+	methodExitLog  = "EXIT CTYPES METHOD"
+	apiMessageLog  = "SEND API MESSAGE"
+)
+
+type CTypeParams struct {
+	Key   string
+	Value string
+}
+
 type CType interface {
 	// For any fields accessing params via copy or alias,
-	// log the key and value of the corresponding param
+	// return the key and value of the corresponding params
 	// (maybe also for fields set "because of" a param)
-	LogCTypeParams()
+	CTypeParams() []CTypeParams
 }
 
 // info on caller of function that called this one
@@ -27,21 +43,24 @@ func GetCaller() runtime.Frame {
 	return frame
 }
 
+// Log method name and params
 func LogCTypesMethodEntry(ctype CType) {
 	w := csv.NewWriter(os.Stdout)
-	// Log method name
-	w.WriteAll([][]string{
-		{"ENTER CTYPES METHOD", GetCaller().Func.Name()},
-	})
 
-	// Log params
-	ctype.LogCTypeParams()
+	params, err := json.Marshal(ctype.CTypeParams())
+	if err != nil {
+		log.Panicf("marshaling %v: %v\n", ctype.CTypeParams(), err.Error())
+	}
+	w.WriteAll([][]string{
+		{methodEntryLog, GetCaller().Func.Name(),
+			string(params)},
+	})
 }
 
 func LogCTypesMethodExit() {
 	w := csv.NewWriter(os.Stdout)
 	w.WriteAll([][]string{
-		{"EXIT CTYPES METHOD", GetCaller().Func.Name()},
+		{methodExitLog, GetCaller().Func.Name()},
 	})
 }
 
@@ -52,7 +71,7 @@ func LogAction(action Action) {
 	api_call_id := fmt.Sprintf("API: k8s.io, TYPE: REQUEST, VERB: %v RESOURCE.SUB: %v.%v NAMESPACE: %v", action.GetVerb(), action.GetResource().String(), action.GetSubresource(), action.GetNamespace())
 
 	w.WriteAll([][]string{
-		{"SEND API MESSAGE", api_call_id},
+		{apiMessageLog, api_call_id},
 	})
 
 	// Message contents (TODO format as csv: key,value)
@@ -76,4 +95,132 @@ func LogAction(action Action) {
 	case ProxyGetActionImpl:
 		fmt.Printf("%+v\n", concrete_action)
 	}
+}
+
+type DataFlow struct {
+	paramKey string
+	msgField string
+}
+type testMethod struct {
+	test   string
+	method string
+}
+
+// A CType method and corresponding params
+type MethodParams struct {
+	method string
+	params []CTypeParams
+}
+type APIMessageInfo struct {
+	controlFlow map[string][]testMethod   // param key => tests that found CF from param to msg
+	dataFlow    map[DataFlow][]testMethod // {param key, msg field} => tests that found DF from param to msg field
+}
+
+// Taint info for each msg gathered across all tests (API call ID => influence)
+type AllTaint map[string]APIMessageInfo
+
+// Eventually may want something more graphable
+func (m *AllTaint) prettyPrint(filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	/*
+			// TODO
+		for api_call_id, info := range m {
+				 format:
+				 	API call
+						CF
+							Param key
+								Test/method
+									Param value
+								<repeat>
+						DF
+							Msg field
+								Param key
+									Test/method
+										Param value (same as field value)
+									<repeat>
+	*/
+	return nil
+}
+
+func (m *AllTaint) add(test string, api_call_id string, params []MethodParams) {
+	// Can't edit map value in place => get it (initializing its maps if needed) and put it back
+	existing_flow := (*m)[api_call_id]
+	if existing_flow.controlFlow == nil {
+		existing_flow.controlFlow = make(map[string][]testMethod)
+	}
+	if existing_flow.dataFlow == nil {
+		existing_flow.dataFlow = make(map[DataFlow][]testMethod)
+	}
+
+	for _, methodParam := range params {
+		for _, param := range methodParam.params {
+
+			/* LEFT OFF learn about them selectors confirm this is right -
+			// Treat selectors.role as a prefix to name the label and field by the role they correspond to, not as its own param
+			*/
+			// CF: Msg is CF-tainted by all params
+			existing_flow.controlFlow[param.Key] = append(existing_flow.controlFlow[param.Key],
+				testMethod{test, methodParam.method})
+
+			// DF: Msg field is DF-tainted by any params whose content match the field
+			// TODO compare params to each message field
+		}
+
+	}
+	(*m)[api_call_id] = existing_flow
+}
+
+func ParseTestOutput(filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	r := csv.NewReader(file)
+	// Allow variable number of fields
+	r.FieldsPerRecord = -1
+
+	// Stack of in-scope methods and their params (last element = most recent)
+	cur_ctype_params := []MethodParams{}
+	msg_taint := make(AllTaint)
+	cur_test := ""
+
+	for {
+		row, err := r.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Println(row)
+				continue
+			}
+		}
+
+		// Enter method
+		if row[0] == methodEntryLog {
+			// TODO nested methods: if same CType, doesn't matter - but if different CTypes, take union of params?
+			params := []CTypeParams{}
+			err := json.Unmarshal([]byte(row[2]), &params)
+			if err != nil {
+				log.Panicf("unmarshaling %v: %v\n", row[2], err.Error())
+			}
+			cur_ctype_params = append(cur_ctype_params, MethodParams{method: row[1], params: params})
+		} else if row[0] == methodExitLog {
+			cur_ctype_params = cur_ctype_params[:len(cur_ctype_params)-1]
+		} else if row[0] == apiMessageLog {
+			msg_taint.add(cur_test, row[1], cur_ctype_params)
+		} else if strings.HasPrefix(row[0], "=== RUN") {
+			fields := strings.Fields(row[0])
+			test := fields[len(fields)-1]
+			cur_test = test
+		} else {
+			// Some other test log
+		}
+	}
+
+	return nil
 }
