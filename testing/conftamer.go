b97d8e2a8cb1216c4755e71864d871176538e455
Emily Marx <emilykmarx@alumni.stanford.edu>
2026-04-24 16:38:33 -0700

    Log message contents using new generic log function in conftamer repo.

--- testing/conftamer.go
+++ testing/conftamer.go
@@ -1,7 +1,6 @@
 package testing
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/emilykmarx/conftamer"
@@ -10,42 +9,17 @@ import (
 /* Call conftamer's message log function for this Action. */
 func LogAction(action Action) {
 	api_call_id := conftamer.APICallID{
-		API:      "k8s.io",
-		Verb:     strings.ToUpper(action.GetVerb()),
-		Resource: action.GetResource().Resource,
+		API:  "k8s.io",
+		Verb: strings.ToUpper(action.GetVerb()),
+		// Unsure yet if resource group and version should be part of the resource used in API call ID, or just the resource type (e.g. pods)
+		Resource: action.GetResource().String(),
 		// TODO will this ever be called for responses?
 		APIMessageType: conftamer.Request,
 	}
 
-	// Contents that apply to all message types
-	// TODO should subresource be contents or type?
-	msg_contents := []conftamer.MsgField{
-		{Key: "namespace", Value: action.GetNamespace()},
-		{Key: "subresource", Value: action.GetSubresource()},
-	}
-
-	switch concrete_action := action.(type) {
-	case ListActionImpl:
-		// LEFT OFF append contents for each type (for now - there may be a more generic way of doing this)
-		// ListRestrictions is redundant with ListOptions
-		fmt.Printf("%+v\n", concrete_action)
-	case GetActionImpl:
-		fmt.Printf("%+v\n", concrete_action)
-	case CreateActionImpl:
-		fmt.Printf("%+v\n", concrete_action)
-	case UpdateActionImpl:
-		fmt.Printf("%+v\n", concrete_action)
-	case DeleteActionImpl:
-		fmt.Printf("%+v\n", concrete_action)
-	case PatchActionImpl:
-		fmt.Printf("%+v\n", concrete_action)
-	case DeleteCollectionActionImpl:
-		fmt.Printf("%+v\n", concrete_action)
-	case WatchActionImpl:
-		fmt.Printf("%+v\n", concrete_action)
-	case ProxyGetActionImpl:
-		fmt.Printf("%+v\n", concrete_action)
-	}
-
+	// Remove Action fields that are part of API call ID
+	exclude := map[string]struct{}{"Verb": {}, "Resource": {}}
+	// The API probably does fancier serialization than this, but doing it generically seems fine for now
+	msg_contents := conftamer.ParseJSONFields(action, exclude)
 	conftamer.LogAPIMessage(api_call_id, msg_contents)
 }
