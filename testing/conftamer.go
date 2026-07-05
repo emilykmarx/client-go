4db1a644a9073d944b8af0b78e1b4eba20ea181a
Emily Marx <emilykmarx@alumni.stanford.edu>
2026-07-05 16:44:07 -0700

    GetMessageInfo: Add leading slash to resource to match Prometheus.

--- testing/conftamer.go
+++ testing/conftamer.go
@@ -8,8 +8,6 @@ import (
 	"github.com/go-delve/delve/service/rpc2"
 )
 
-// XXX this is specific to k8s - plan is for module dev to import the lib containing their msgInfo here,
-// and pass name of msgInfo func
 func GetMessageInfo(client *rpc2.RPCClient, bp *api.Breakpoint, goroutine int64) (*apimessages.APICallID, error) {
 	loadcfg := api.LoadConfig{FollowPointers: true, MaxVariableRecurse: 10, MaxStringLen: 10, MaxArrayValues: 1, MaxStructFields: -1}
 	scope := api.EvalScope{GoroutineID: goroutine}
@@ -26,11 +24,10 @@ func GetMessageInfo(client *rpc2.RPCClient, bp *api.Breakpoint, goroutine int64)
 		API:  "k8s.io",
 		Verb: strings.ToUpper(verb.Value),
 		// Unsure yet if resource group and version should be part of the resource used in API call ID, or just the resource type (e.g. pods)
-		Resource: resource.Value,
+		Resource: "/" + resource.Value,
 		// TODO will this ever be called for responses?
 		APIMessageType: apimessages.Request,
 	}
 
-	// XXX get contents
 	return &api_call_id, nil
 }
